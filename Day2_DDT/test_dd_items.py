50b7b609a59be50e7f0ae6a95f16a34b25a867be
DurgeshYaduvanshi2026 <Duggu@Shikha2024>
2026-09-15 22:16:09 +0530

    Update the test cases

--- Day2_DDT/test_dd_items.py
+++ Day2_DDT/test_dd_items.py
@@ -3,6 +3,7 @@ import pytest
 from playwright.sync_api import expect, Playwright, Page
 
 search_items = ["laptop", "Gift card", "smartphone", "camera"]
+search_items = ["laptop", "Gift card", "smartphone", "camera"]
 
 
 @pytest.mark.parametrize("item", search_items)
